55174c549ef9a786cbc568791412db1c47881ce3
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2025-05-10 17:53:28 +0530

    tempery sqlite used for testing and demo purposes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config");
 
-const sequelize = new Sequelize(config.database.url, {
+const sequelize1 = new Sequelize(config.database.url, {
   dialect: "postgres",
   logging: false,
+});
+
+const sequelize = new Sequelize('task_management', 'sagar', 'password123', {
+  host: 'localhost',
+  dialect: 'sqlite', // or 'sqlite', 'postgres', 'mssql'
+  logging: false, // Disable logging; default: console.log
+  storage: './sqlite/database.sqlite', // SQLite only
+  dialectOptions: {
+    // SQLite only
+    useUTC: false, // Prevents the date from being converted to UTC
+  },
+
 });
 
 module.exports = sequelize;
